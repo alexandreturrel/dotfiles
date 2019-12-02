2105661732f581dd07d78ed82a7c713f6f303dae
Alexandre Turrel <11587932+alexandreturrel@users.noreply.github.com>
2019-12-02 18:23:41 +0100

    Update adbscreen.bat
    Add "how to" in comments

--- adbscreen.bat
+++ adbscreen.bat
@@ -5,3 +5,8 @@ set nb=%1
 adb shell screencap -p /sdcard/screen.png
 adb pull /sdcard/screen.png screen%nb%.png
 adb shell rm /sdcard/screen.png
+
+
+::    How to execute:
+::    In a Windows cmd or Powershell, navigate to the right folder and
+::    adbscreen.bat IP
